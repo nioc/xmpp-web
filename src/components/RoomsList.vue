f36664b833f66a2dfdc8bbaf0eb886abeb03708a
nioc <dev@nioc.eu>
2026-03-01 01:14:52 +0100

    feat(muc): improve of UI for public room list (adding search)

--- src/components/RoomsList.vue
+++ src/components/RoomsList.vue
@@ -1,6 +1,14 @@
 <template>
   <main class="section">
     <h2 class="title">Public rooms</h2>
+    <div class="field has-addons">
+      <div class="control is-expanded has-icons-left">
+        <input v-model="search" class="input" type="text" name="room" placeholder="Search a room" title="Filter rooms by name or description">
+        <span class="icon is-small is-left">
+          <i class="fa-solid fa-magnifying-glass" />
+        </span>
+      </div>
+    </div>
     <table class="table is-hoverable is-fullwidth center-table">
       <thead>
         <tr>
@@ -11,19 +19,17 @@
           <th class="has-text-centered">Moderated</th>
           <th class="has-text-centered">Semi-Anonymous</th>
           <th class="has-text-centered">Members-Only</th>
-          <th class="has-text-centered">Visibility</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="room in publicRooms" :key="room.jid" class="is-clickable" @click="openRoom(room.jid)">
+        <tr v-for="room in filteredPublicRooms" :key="room.jid" class="is-clickable" :title="getRoomTitle(room)" @click="openRoom(room.jid)">
           <td><avatar v-if="room.hasVCard" :jid="room.jid" :size="32" /></td>
-          <td class="has-text-centered" :title="getRoomTitle(room)">{{ room.name }}</td>
+          <td class="has-text-centered">{{ room.name }}</td>
           <td class="has-text-centered">{{ room.occupantsCount }}</td>
           <td class="has-text-centered"><i v-if="room.isPasswordProtected" class="fa-solid fa-key fa-fw" title="Password protected" /></td>
           <td class="has-text-centered"><i v-if="room.isModerated" class="fa-solid fa-microphone-lines-slash fa-fw" title="Is moderated" /></td>
           <td class="has-text-centered"><i v-if="room.isAnonymous" class="fa-solid fa-user-secret fa-fw" title="Allow anonymous (nick)" /></td>
           <td class="has-text-centered"><i v-if="room.isMembersOnly" class="fa-solid fa-user-lock fa-fw" title="Restricted to members" /></td>
-          <td class="has-text-centered"><i v-if="room.isPublic" class="fa-solid fa-compass fa-fw" title="Public room" /></td>
         </tr>
       </tbody>
     </table>
@@ -40,7 +46,17 @@ export default {
   components: {
     avatar,
   },
+  data () {
+    return {
+      search: '',
+    }
+  },
   computed: {
+    filteredPublicRooms () {
+      const searchText = this.search.toLowerCase()
+      return this.publicRooms
+        .filter((room) => (searchText === '' || room.name.toLowerCase().indexOf(searchText) > -1 || room.description.toLowerCase().indexOf(searchText) > -1))
+    },
     ...mapState(useStore, ['publicRooms']),
   },
   async mounted () {
